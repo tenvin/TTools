20528e8d81f3de268566925ab34e9992abb60f0f
tenvin <tenvin@gmail.com>
2016-03-12 21:22:24 +0800

    test case

---
+++ src/test/java/com/twg/ttools/utils/DateUtilsTest.java
@@ -0,0 +1,27 @@
+package com.twg.ttools.utils;
+
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class DateUtilsTest {
+
+    /**
+     * Method: countDays(Date begin, Date end)
+     */
+    @Test
+    public void testCountDays() throws Exception {
+        SimpleDateFormat myFormatter = new SimpleDateFormat("yyyy-MM-dd");
+        Date begin= myFormatter.parse("2016-02-01");
+        Date end= myFormatter.parse("2016-03-01");
+
+        System.out.println(DateUtils.countDays(begin,end));
+    }
+
+
+} 
