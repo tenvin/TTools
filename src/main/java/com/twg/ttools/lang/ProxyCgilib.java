512ec66f1a912e7464b0991d1843ce0de2db89de
tenvin <tenvin@gmail.com>
2017-06-22 09:13:07 +0800

    proxy

---
+++ src/main/java/com/twg/ttools/lang/ProxyCgilib.java
@@ -0,0 +1,42 @@
+package com.twg.ttools.lang;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by twg on 2017/6/22.
+ */
+public class ProxyCgilib implements MethodInterceptor {
+    private Object target;
+
+    /**
+     * 创建代理对象
+     *
+     * @param target
+     * @return
+     */
+    public Object getInstance(Object target) {
+        this.target = target;
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(this.target.getClass());
+        // 回调方法
+        enhancer.setCallback(this);
+        // 创建代理对象
+        return enhancer.create();
+    }
+
+    @Override
+    // 回调方法
+    public Object intercept(Object obj, Method method, Object[] args,
+                            MethodProxy proxy) throws Throwable {
+        System.out.println("pre");
+        proxy.invokeSuper(obj, args);
+        System.out.println("post");
+        return null;
+
+
+    }
+}
